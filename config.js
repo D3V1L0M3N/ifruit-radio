826c2e1c66c523f655bbcf12ef3940fa2b01acdf
D3V1L0M3N <60878177+D3V1L0M3N@users.noreply.github.com>
2022-09-06 20:20:18 +0930

    Add station definitions

--- config.js
+++ config.js
@@ -2,6 +2,43 @@ module.exports = {
   // color style of message embeds
   themeColor: '#eb2fde',
 
-
+  // define all existing stations
+  stations: [
+    {
+      id: 'los_santos_rock_radio',
+      name: 'Los Santos Rock Radio',
+      description: 'Classic Rock',
+      emoji: '<:los_santos_rock_radio:938050073177239602>',
+      streamURL: 'https://station.throwback.fm:8010/radio.mp3'
+    },
+    {
+      id: 'west_coast_classics',
+      name: 'West Coast Classics',
+      description: 'Hip Hop',
+      emoji: '<:west_coast_classics:990609369207029791>',
+      streamURL: 'https://station.throwback.fm:8020/radio.mp3'
+    },
+    {
+      id: 'nonstop_pop_fm',
+      name: 'Nonstop Pop FM',
+      description: 'Pop',
+      emoji: '<:nonstop_pop_fm:990623355679944764>',
+      streamURL: 'https://station.throwback.fm:8030/radio.mp3'
+    },
+    {
+      id: 'rebel_radio',
+      name: 'Rebel Radio',
+      description: 'Country',
+      emoji: '<:rebel_radio:990630569215926342>',
+      streamURL: 'https://station.throwback.fm:8040/radio.mp3'
+    },
+    {
+      id: 'radio_mirror_park',
+      name: 'Radio Mirror Park',
+      description: 'Indie Music',
+      emoji: '<:radio_mirror_park:990637772425560106>',
+      streamURL: 'https://station.throwback.fm:8050/radio.mp3'
+    }
+  ]
 
 }
